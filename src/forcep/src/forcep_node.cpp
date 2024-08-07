2ceb9215b6ee5c0fa9e58e4678b1a6862ce6ce39
itt-icc <1693933133@qq.com>
2024-08-07 21:10:50 +0800

    20240806

--- src/forcep/src/forcep_node.cpp
+++ src/forcep/src/forcep_node.cpp
@@ -40,7 +40,7 @@ using namespace std;
 
 //}
 
-GReturn Forcep(GCon g)
+GReturn Forcep_right(GCon g)
 {
     char buf[G_SMALL_BUFFER]; // traffic buffer
 
@@ -140,6 +140,106 @@ GReturn Forcep(GCon g)
     return GALIL_EXAMPLE_OK;
 }
 
+GReturn Forcep_left(GCon g)
+{
+    char buf[G_SMALL_BUFFER]; // traffic buffer
+
+    char forcep_axis = 'B';
+    char rotate_axis = 'E';
+    int position_hold = 6700; // TODO need calibration
+    int position_release = 2500;
+    int grasp_rotate_speed = 800;
+
+    StopMotor(g, forcep_axis);
+
+    GoHome2(g, forcep_axis); // first triggered photoelectric gate
+
+    GoPosition(g, forcep_axis, position_release, grasp_rotate_speed); // then go to init position
+
+    galil(GMotionComplete(g, "B")); // Wait for motion to complete
+    galil(GCmd(g, "STB"));          // stop motor
+    galil(GMotionComplete(g, "B")); // Wait for motion to complete
+    // galil(GCmd(g, "DPC=0"));
+    galil(GCmd(g, "PTB=1"));
+    galil(GCmd(g, "SPB=1000"));  // speed
+    galil(GCmd(g, "ACB=20000")); // acceleration
+    galil(GCmd(g, "DCB=20000")); // deceleration
+
+    // galil(GCmd(g, "PTD=1"));  // JOG is fail to work
+    galil(GCmd(g, "SPE=1000"));  // speed
+    galil(GCmd(g, "ACE=20000")); // acceleration
+    galil(GCmd(g, "DCE=20000")); // deceleration
+    galil(GCmd(g, "JGE=0"));     // set D axis Jog speed=0
+    galil(GCmd(g, "BG E;"));     // Begin Jog
+    std::cout << "option: 1--forcep clamping/2--forcep release/3--forcep rotating/else--stop rotating" << std::endl;
+
+    int D_rotate_speed = 1000;
+    //    int position_now = position_release;
+    while (1)
+    {
+        // std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+        /* GET INPUT */
+        int option;
+        std::cin >> option;
+        std::cout << "option: " << option << std::endl;
+
+        /* Forcep Control */
+        if (option == 1)
+        {
+            sprintf(buf, "PA%c=%d", forcep_axis, position_hold);
+            //            position_now+=20;
+            //            sprintf(buf, "PA%c=%d",forcep_axis,position_now);
+            galil(GCmd(g, buf)); // position relative
+            std::cout << "clamping" << std::endl;
+        }
+        else if (option == 2)
+        {
+            sprintf(buf, "PA%c=%d", forcep_axis, position_release);
+            //            position_now-=20;
+            //            sprintf(buf, "PA%c=%d",forcep_axis,position_now);
+            galil(GCmd(g, buf)); // position relative
+            std::cout << "release" << std::endl;
+        }
+        // else if(option == 3){
+        //     sprintf(buf, "PA%c=%d",rotate_axis,position_hold);
+
+        //     galil(GCmd(g, buf)); // position relative
+        //     std::cout << "rotate point1"<<std::endl;
+        // }
+        // else if(option == 4){
+        //     sprintf(buf, "PA%c=%d",rotate_axis,position_release);
+
+        //     galil(GCmd(g, buf)); // position relative
+        //     std::cout << "rotate point2"<<std::endl;
+        // }
+
+        /* Rotate Control */
+        if (option == 3)
+        {
+            Jog(g, 'E', 0);
+            Jog(g, 'E', D_rotate_speed);
+        }
+        else if (option == 4)
+        {
+            Jog(g, 'E', 0);
+            Jog(g, 'E', D_rotate_speed * -1);
+        }
+        else if (option == 5)
+        {
+            Jog(g, 'E', 0);
+        }
+        // loop end
+        //        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        //        std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+        //        if(10000.0-time_span.count()*1000000>0){
+        //            usleep(10000.0-time_span.count()*1000000);
+        //        }
+    }
+    return GALIL_EXAMPLE_OK;
+}
+
 int main2()
 {
     GReturn rc = GALIL_EXAMPLE_OK;
@@ -155,7 +255,7 @@ int main2()
         sprintf(buf, "%s --subscribe MG", address);
         galil(GOpen(buf, &g)); // Opens a connection at the provided address
 
-        rc = Forcep(g);
+        rc = Forcep_right(g);
     }
     catch (GReturn gr)
     {
@@ -168,31 +268,49 @@ int main2()
     return GALIL_EXAMPLE_OK;
 }
 
-void joint_states_callback(const sensor_msgs::JointState::ConstPtr joint_states)
+void left_joint_states_callback(const sensor_msgs::JointState::ConstPtr joint_states)
 {
     ROS_INFO("join = [%.3f] [%.3f] [%.3f] [%.3f] [%.3f] [%.3f]", joint_states->position[0], 
     joint_states->position[1], joint_states->position[2], joint_states->position[3], 
     joint_states->position[4], joint_states->position[5]);
 }
 
-void button_callback(const omni_msgs::OmniButtonEvent::ConstPtr button_states){
+void left_button_callback(const omni_msgs::OmniButtonEvent::ConstPtr button_states){
     ROS_INFO("white = [%d] gray = [%d]",button_states->white_button,button_states->grey_button);
+}
 
+void right_joint_states_callback(const sensor_msgs::JointState::ConstPtr joint_states)
+{
+    ROS_INFO("join = [%.3f] [%.3f] [%.3f] [%.3f] [%.3f] [%.3f]", joint_states->position[0], 
+    joint_states->position[1], joint_states->position[2], joint_states->position[3], 
+    joint_states->position[4], joint_states->position[5]);
+}
+
+void right_button_callback(const omni_msgs::OmniButtonEvent::ConstPtr button_states){
+    ROS_INFO("white = [%d] gray = [%d]",button_states->white_button,button_states->grey_button);
 }
 
 int main(int argc, char *argv[])
 {
-
     ros::init(argc, argv, "forcep_node");
-    std::string joint_states_topic;
-    ros::param::param(std::string("~joint_states_topic"), joint_states_topic, std::string("/left_device/phantom/joint_states"));
     ros::NodeHandle nh;
-    ros::Subscriber joint_sub = nh.subscribe<sensor_msgs::JointState>(joint_states_topic.c_str(), 1, joint_states_callback);
 
-
+    std::string joint_states_topic;
     std::string button_topic;
+
+    ros::param::param(std::string("~joint_states_topic"), joint_states_topic, std::string("/left_device/phantom/joint_states"));
     ros::param::param(std::string("~button_topic"), button_topic, std::string("/left_device/phantom/button"));
-    ros::Subscriber button_sub = nh.subscribe<omni_msgs::OmniButtonEvent>(button_topic.c_str(), 10, button_callback);
+
+    if(joint_states_topic.find("left") != std::string::npos){ //"left" in joint_states_topic
+        ros::Subscriber joint_sub = nh.subscribe<sensor_msgs::JointState>(joint_states_topic.c_str(), 1, left_joint_states_callback);
+        ros::Subscriber button_sub = nh.subscribe<omni_msgs::OmniButtonEvent>(button_topic.c_str(), 10, left_button_callback);
+    }
+    else if(joint_states_topic.find("right") != std::string::npos){ //"right" in joint_states_topic
+        ros::Subscriber joint_sub = nh.subscribe<sensor_msgs::JointState>(joint_states_topic.c_str(), 1, right_joint_states_callback);
+        ros::Subscriber button_sub = nh.subscribe<omni_msgs::OmniButtonEvent>(button_topic.c_str(), 10, right_button_callback);
+    }
+
+
 
     ros::spin();
 
